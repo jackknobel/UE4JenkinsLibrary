0ad1b5f0e5b772d969fef93afccad114b528abbd
Jack Knobel <jknobel>
2018-03-22 12:18:46 +1100

    Reverting back to original class based method

--- src/unreal/UE4.groovy
+++ src/unreal/UE4.groovy
@@ -10,51 +10,52 @@ enum CompilationConfig
 	Shipping
 }
 
-// UE4 Default Paths
-def UBT		= '/Engine/Build/BatchFiles/Build.bat'
-def UAT		= '/Engine/Build/BatchFiles/RunUAT.bat'
-def UE4CMD	= '/Engine/Binaries/Win64/UE4Editor-Cmd.exe'
-
-// Project Specific Directories
-def ProjectName = ''
-def ProjectUBT	= ''
-def EditorCMD	= ''
-def ProjectDir	= ''
-def ProjectFile	= ''
-
-def Initialise(String projectName, String workingRoot)
+class UE4
 {
-	EngineUBT          = "${workingRoot}${UBT}"
-	EditorCMD          = "${workingRoot}${UE4CMD}"
-	ProjectDir         = "${workingRoot}/${ProjectName}"
-	ProjectFile        = "${ProjectDir}/${ProjectName}.uproject"
-}
+	// UE4 Default Paths
+	def UBT		= '/Engine/Build/BatchFiles/Build.bat'
+	def UAT		= '/Engine/Build/BatchFiles/RunUAT.bat'
+	def UE4CMD	= '/Engine/Binaries/Win64/UE4Editor-Cmd.exe'
+	
+	// Project Specific Directories
+	def ProjectName = ''
+	def ProjectUBT	= ''
+	def EditorCMD	= ''
+	def ProjectDir	= ''
+	def ProjectFile	= ''
 
-def GenerateProjectFiles()
-{
-	bat "${EngineUBT} -projectfiles -project=${ProjectFile} -game -engine -progress"
-}
-
-// Replace with enum for config type???
-def CompileProject(CompilationConfig compilationConfig, boolean editor = true, String platform = "win64")
-{
-	compilationTarget = "${ProjectName}"
-	if(compilationConfig <= CompilationConfig.Development && editor)
+	def Initialise(String projectName, String workingRoot)
 	{
-		compilationTarget += "Editor"
+		EngineUBT          = "${workingRoot}${UBT}"
+		EditorCMD          = "${workingRoot}${UE4CMD}"
+		ProjectDir         = "${workingRoot}/${ProjectName}"
+		ProjectFile        = "${ProjectDir}/${ProjectName}.uproject"
 	}
-	bat "${EngineUBT} ${compilationTarget} ${ProjectFile}" + compilationTarget.name() + "${platform}"
-}
+	
+	def GenerateProjectFiles()
+	{
+		bat "${EngineUBT} -projectfiles -project=${ProjectFile} -game -engine -progress"
+	}
+	
+	// Replace with enum for config type???
+	def CompileProject(CompilationConfig compilationConfig, boolean editor = true, String platform = "win64")
+	{
+		compilationTarget = "${ProjectName}"
+		if(compilationConfig <= CompilationConfig.Development && editor)
+		{
+			compilationTarget += "Editor"
+		}
+		bat "${EngineUBT} ${compilationTarget} ${ProjectFile}" + compilationTarget.name() + "${platform}"
+	}
+	
+	// Each platform should be seperated by a +. e.g. WindowsNoEditor+Xbox+Linux 
+	def CookProject(String platforms = "WindowsNoEditor", String additionalArguments = "-iterate")
+	{
+		 bat "${EditorCMD} -run=Cook -project=${ProjectFile} -targetplatform=${platforms} ${additionalArguments}"
+	}
 
-// Each platform should be seperated by a +. e.g. WindowsNoEditor+Xbox+Linux 
-def CookProject(String platforms = "WindowsNoEditor", String additionalArguments = "-iterate")
-{
-	 bat "${EditorCMD} -run=Cook -project=${ProjectFile} -targetplatform=${platforms} ${additionalArguments}"
+	def BuildDDC()
+	{
+		 bat "%EDITOR_CMD% -run=DerivedDataCache -fill -project=%PROJECT_FILE%"
+	}
 }
-
-def BuildDDC()
-{
-	 bat "%EDITOR_CMD% -run=DerivedDataCache -fill -project=%PROJECT_FILE%"
-}
-
-return this

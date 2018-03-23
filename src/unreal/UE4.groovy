45a967d50e1518223a6ababf9fc9437ccc5bf4ce
Jack Knobel <jknobel>
2018-03-24 06:46:44 +1100

    Deploy changes to make pak an optional argument and allowing DeployXbox to specific pak settings

--- src/unreal/UE4.groovy
+++ src/unreal/UE4.groovy
@@ -101,14 +101,14 @@ def CookProject(String platforms = "WindowsNoEditor", String mapsToCook = "", bo
 /** 
   * Deploy the project to a platform
   * buildConfiguration - The BuildConfiguration type of this deployment
-  * usePak - Whether or not to use pak files
   *	outputDir - The staging directory we want to output this deployment to
+  * usePak - Whether or not to use pak files
   * iterative - Use iterative deployment
   * additionalArguments - Optional arguments to pass to the deployment command
  */ 
-def Deploy(String platform, BuildConfiguration buildConfiguration, boolean usePak, String outputDir, boolean iterative = true, String additionalArguments = "")
+def Deploy(String platform, BuildConfiguration buildConfiguration, String outputDir, boolean usePak = true, boolean iterative = true, String additionalArguments = "")
 {
-	bat "${EngineUAT} BuildCookRun -project=${ProjectFile} -platform=${platform} -skipcook -skipbuild -nocompileeditor -NoSubmit -stage -package -clientconfig=" + buildConfiguration.name() + (usePak ? " -pak " : " ") + additionalArguments + " -StagingDirectory=\"${outputDir}\"" +  (iterative ? " -iterativedeploy" : "")
+	bat "${EngineUAT} BuildCookRun -project=${ProjectFile} -platform=${platform} -skipcook -skipbuild -nocompileeditor -NoSubmit -stage -package -clientconfig=" + buildConfiguration.name() + additionalArguments + " -StagingDirectory=\"${outputDir}\"" + (usePak ? " -pak " : " ") +  (iterative ? " -iterativedeploy" : "")
 }
 
 /** 
@@ -116,12 +116,13 @@ def Deploy(String platform, BuildConfiguration buildConfiguration, boolean usePa
   * consoleIP - The IP of the console we want to deploy to
   * buildConfiguration - The BuildConfiguration type of this deployment
   *	outputDir - The staging directory we want to output this deployment to
+  * usePak - Whether or not to use pak files
   * iterative - Use iterative deployment
   * additionalArguments - Optional arguments to pass to the deployment command
  */ 
-def DeployXbox(String consoleIP, BuildConfiguration buildConfiguration, String outputDir, boolean iterative = true, String additionalArguments = "")
+def DeployXbox(String consoleIP, BuildConfiguration buildConfiguration, String outputDir, boolean usePak = true, boolean iterative = true, String additionalArguments = "")
 {
-	Deploy("XboxOne", buildConfiguration, true, outputDir, iterative, "-cmdline=-Messaging -device=XboxOne@" + consoleIP + " " + additionalArguments)
+	Deploy("XboxOne", buildConfiguration, true, outputDir, usePak, iterative, "-cmdline=-Messaging -device=XboxOne@" + consoleIP + " " + additionalArguments)
 }
 
 /* Build the project's DDC, recommend to use in combation with a shared DDC https://docs.unrealengine.com/en-us/Engine/Basics/DerivedDataCache */

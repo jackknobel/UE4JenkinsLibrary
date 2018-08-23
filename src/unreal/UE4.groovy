a05ab61c26b2bf4412d7e93905a599276318f78e
Jack Knobel <jknobel@me.com>
2018-08-23 16:17:59 +1000

    Changed Project Argument to UProject

--- src/unreal/UE4.groovy
+++ src/unreal/UE4.groovy
@@ -99,7 +99,7 @@ def RunBuildGraph(String scriptPath, String target, def parameters, boolean clea
 	{
 		parameter -> parsedParams += "-set:${parameter.key}=\"${parameter.value}\" "
 	}
-	bat "${EngineUAT} BuildGraph -Script=\"${scriptPath}\" -target=\"${target}\" -set:ProjectName=${ProjectName} -set:Project=${ProjectFile} ${parsedParams} ${additionalArguments} ${DefaultArguments} " + (clearHistory ? "-ClearHistory" : "")
+	bat "${EngineUAT} BuildGraph -Script=\"${scriptPath}\" -target=\"${target}\" -set:ProjectName=${ProjectName} -set:UProject=${ProjectFile} ${parsedParams} ${additionalArguments} ${DefaultArguments} " + (clearHistory ? "-ClearHistory" : "")
 }
 
 /** 

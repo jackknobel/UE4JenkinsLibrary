18aa39d3ac8838b2d9ff2f64ae0b44060b4785e9
Jack Knobel <jknobel@me.com>
2018-08-23 17:05:26 +1000

    Attempting trim and added logging

--- src/unreal/UE4.groovy
+++ src/unreal/UE4.groovy
@@ -100,7 +100,7 @@ def RunBuildGraph(String scriptPath, String target, def parameters, boolean clea
 		parameter -> parsedParams += "-set:${parameter.key}=\"${parameter.value}\" "
 	}
 
-	parsedParams.trim()
+	parsedParams = parsedParams.trim()
 
 	bat "${EngineUAT} BuildGraph -Script=\"${scriptPath}\" -target=\"${target}\" -set:ProjectName=${ProjectName} -set:UProject=${ProjectFile} ${parsedParams} ${additionalArguments} ${DefaultArguments} " + (clearHistory ? "-ClearHistory" : "")
 }
